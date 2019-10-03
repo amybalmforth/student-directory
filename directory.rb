dacf2e6ac0c857bc715f48143adc5a502ed7e5bc
Amy Balmforth <amy.balmforth@gmail.com>
2019-10-03 15:28:29 +0100

    Refactoring

--- directory.rb
+++ directory.rb
@@ -1,22 +1,3 @@
-# Let's put all the students into an array
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
-# And then iterate them
-students.each do |student|
-  puts student
-end
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
