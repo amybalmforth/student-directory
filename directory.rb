0da911dcb68f3d1784da31d36e72fd59f5b4a1a2
Amy Balmforth <amy.balmforth@gmail.com>
2019-10-04 16:43:34 +0100

    Exercise 7

--- directory.rb
+++ directory.rb
@@ -14,6 +14,7 @@ def print_footer(students)
 end
 
 def input_students
+  existing_cohorts = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
   puts "Please enter the name of a student"
   puts "To finish, just hit return twice"
   # create an empty array
@@ -24,8 +25,23 @@ def input_students
   while !name.empty? do
     puts "Please enter the student's country of origin"
     country = gets.chomp
+      if country == ""
+        country = "UK"
+      end
+    puts "Please enter the student's cohort"
+    cohort = gets.chomp.downcase
+      if cohort == ""
+        cohort = "november"
+      end
+      while !existing_cohorts.include?(cohort.downcase) do
+        puts "Sorry, that cohort doesn't exist, please try again"
+        cohort = gets.chomp.downcase
+        if existing_cohorts.include?(cohort.downcase)
+          break
+        end
+      end
     # add the student hash to the array
-    students << {name: name, country: country, cohort: :november}
+    students << {name: name.to_sym, country: country.to_sym, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students, enter another name or press return to finish"
     # get another name from the user
     name = gets.chomp
