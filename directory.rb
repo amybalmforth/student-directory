c1de978e89e276b503a020a5381e5dd16fa76c41
Amy Balmforth <amy.balmforth@gmail.com>
2019-10-07 20:31:49 +0100

    Step 9

--- directory.rb
+++ directory.rb
@@ -1,3 +1,24 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header(students)
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
 
 def print_header(students)
   if students.length > 0
@@ -76,7 +97,4 @@ def input_students
   students
 end
 
-students = input_students
-print_header(students)
-print(students)
-print_footer(students)
+interactive_menu
