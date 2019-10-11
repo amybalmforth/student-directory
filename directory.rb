09300ba9cbc44f87786eb01e7b22d169860f4224
Amy Balmforth <amy.balmforth@gmail.com>
2019-10-11 20:12:21 +0100

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,24 +1,24 @@
-
+# require CSV package
 require 'csv'
-
-@students = [] # an empty array accessible to all methods
-
+# an empty array accessible to all methods
+@students = []
+# method to print the menu options
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
   puts "5. Delete the list from students.csv"
-  puts "9. Exit" # because we'll be adding more items
+  puts "9. Exit"
 end
-
+# method to loop back to the menu each time
 def interactive_menu
   loop do
     print_menu
     process(STDIN.gets.chomp)
   end
 end
-
+# method to execute menu options
 def process(selection)
   case selection
   when "1"
@@ -40,7 +40,7 @@ def process(selection)
     puts "I don't know what you mean, try again"
   end
 end
-
+# method to load the student file (you have to specify which file to load)
 def load_students
   puts "Which file do you want to load?"
   file = STDIN.gets.chomp
@@ -49,7 +49,7 @@ def load_students
     write_students
   end
 end
-
+# method to save the student file (you have to specify which file to save)
 def save_students
   puts "Which file do you want to save to?"
   file = STDIN.gets.chomp
@@ -63,7 +63,7 @@ def save_students
     end
   end
 end
-
+# method for inputting new students
 def input_students
   puts "Please enter the names of the students you wish to add"
   puts "To finish, just hit return twice"
@@ -78,38 +78,38 @@ def input_students
     @name = STDIN.gets.chomp
   end
 end
-
+# method for putting the new student name and cohort in the students array
 def write_students
   @students << {name: @name, cohort: :november}
 end
-
+# method to print out the current list of students plus header and footer
 def show_students
   print_header
   print_student_list
   print_footer
 end
-
+# method for printing header
 def print_header
   puts "The students of Villains Academy".center(100)
   puts "-------------".center(100)
 end
-
+# method for printing student list
 def print_student_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100)
   end
 end
-
+# method for printer footer
 def print_footer
   puts "Overall, we have #{@students.count} great students".center(100)
 end
-
-def delete_students # delete the list of students from CSV
+# method for deleting all students from students.CSV
+def delete_students
   CSV.open("students.csv", "w") do |csv|
   @students.clear
   end
 end
-
+# method to check if a file was supplied as an argument from the command line
 def try_load_students
   filename = ARGV.first # first argument from the command line
   return if filename.nil? # get out of the method if it isn't given
